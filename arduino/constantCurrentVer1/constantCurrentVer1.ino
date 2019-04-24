5c155673ac7b6147b542351214f2191665c6a5e7
[hanamichisanada] <datuota00@gmail.com>
2019-04-24 15:40:03 +0900

    Update constantCurrentVer1.ino

--- arduino/constantCurrentVer1/constantCurrentVer1.ino
+++ arduino/constantCurrentVer1/constantCurrentVer1.ino
@@ -1,11 +1,13 @@
-#define aOut A18
+#define aOut A18 //
 
 void setup() {
 }
 
 void loop() {
-  for(int i=0; i<255; i++){
+  
+  for(int i=0; i<=255; i++){
     dacWrite(aOut,i);
     delay(10);
   }
+  
 }
